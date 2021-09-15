bd96f262ff013b8b5006da9f1c542c42e3cbcf78
cbman <38330163+cbman1999@users.noreply.github.com>
2021-09-16 00:41:00 +0300

    Create N2.java

---
+++ First/N2.java
@@ -0,0 +1,19 @@
+// 2. Пользователь вводит числа, в конце вводит 0. Вывести сумму
+
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            System.out.print("Введите число - ");
+            int numbers = scanner.nextInt();
+            if (numbers == 0) {
+                break;
+            }
+            sum += numbers;
+        }
+        System.out.println("Сумма введеных чисел - " + sum);
+    }
+}
