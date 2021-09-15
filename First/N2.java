1fdca645769bfdf4ebab455ce327e49e8e8fa653
cbman <38330163+cbman1999@users.noreply.github.com>
2021-09-16 00:43:39 +0300

    Update N2.java

--- First/N2.java
+++ First/N2.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class first {
+public class N2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
