f988cfb1986406ca71d21538196c76593951a999
cbman <38330163+cbman1999@users.noreply.github.com>
2021-09-19 13:45:54 +0300

    Create N3.java

---
+++ First/N3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int prev = in.nextInt();
+        boolean flag = false;
+        boolean flag2 = false;
+        boolean flag3 = false;
+        int count1 = 0; // <
+        int count2 = 0; // >
+        while (true) {
+            int numbers = in.nextInt();
+            if (prev == 0 || numbers == 0) {
+                break;
+            }
+            if (prev < numbers) {
+                flag = true;
+                count1++;
+            } else if (prev > numbers) {
+                flag2 = true;
+                count2++;
+            } else if (prev == numbers) {
+                flag3 = true;
+            }
+            prev = numbers;
+        } if (flag && flag2) {
+            System.out.println("!");
+        } else if (count2 == 0 && flag) {
+            System.out.print("<");
+        } else if (count1 == 0 && flag2) {
+            System.out.println(">");
+        } else if (count1 == 0 && count2 == 0 && flag3) {
+            System.out.println("=");
+        }
+    }
+}
