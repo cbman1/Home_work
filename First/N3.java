9dbf5e136c7bc1f37b6bdf0989186993bb727ce1
cbman <38330163+cbman1999@users.noreply.github.com>
2021-09-19 13:48:04 +0300

    Update N3.java

--- First/N3.java
+++ First/N3.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class first {
+public class N3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int prev = in.nextInt();
