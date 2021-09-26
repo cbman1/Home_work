d1b1997d624c0db73324a369856cf0cf0b476a43
Ilya Ivlev <38330163+cbman1@users.noreply.github.com>
2021-09-26 17:49:16 +0300

    Create N1.java

---
+++ HW2/N1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class first {
+    public static boolean prime(int a) {
+        if (a > 1) {
+            for (int i = 2; i < Math.sqrt(a); i++) {
+                if (a % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner In = new Scanner(System.in);
+        int a = In.nextInt();
+        if (prime(a)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
