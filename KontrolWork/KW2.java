b6484f72246e8aa779346690a7f71dc92ccd9ba5
Ilya Ivlev <38330163+cbman1@users.noreply.github.com>
2021-10-07 17:09:22 +0300

    Create KW2.java

---
+++ KontrolWork/KW2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class KontrolWork {
+    static boolean CheckChet(int a) {
+        boolean flag = true;
+        while (a > 0) {
+            flag &= ((a % 10) % 2 == 0); {
+                a /= 10;
+
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean answer = false;
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            answer |= CheckChet(a);
+        }
+        if (answer) {
+            System.out.print("Yes");
+        }
+        else {
+            System.out.print("No");
+        }
+    }
+}
